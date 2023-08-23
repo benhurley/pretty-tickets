f801d0abf386f20a3ed71467d90ce625545b9d93
Benjamin Hurley <benfromtech@gmail.com>
2023-08-23 11:25:13 -0400

    make full header logo clickable

--- src/components/organisms/header.tsx
+++ src/components/organisms/header.tsx
@@ -4,8 +4,7 @@ export const Header = () => {
     return (
         <div className="w-full fixed top-0 z-20 text-xs bg-white bg-opacity-90">
             <p className="text-lg font-bold text-gray-700 text-center p-2 transform hover:scale-105 transition-transform duration-300">
-                <Link to="/">Pretty Tickets</Link>
-                <img className="inline-block ml-1 -mt-1" width={30} src="/logo128.png" alt="ticket" />
+                <Link to="/">Pretty Tickets<img className="inline-block ml-1 -mt-1" width={30} src="/logo128.png" alt="ticket" /></Link>
             </p>
         </div>
     )
