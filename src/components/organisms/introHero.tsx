7adfd960ee3dcd53489b10c08c498c07ff0ea8ea
Benjamin Hurley <benfromtech@gmail.com>
2023-08-24 00:22:41 -0400

    use smaller font on home subtitle

--- src/components/organisms/introHero.tsx
+++ src/components/organisms/introHero.tsx
@@ -1,13 +1,13 @@
 export const IntroHero = () => {
     return (
         <div className="max-w-[900px] px-6 pt-10 lg:pt-4 animate-in fade-in zoom-in ease-in-out">
-            <div>
+            <>
                 <h1 className="text-4xl lg:text-6xl font-extrabold leading-tight text-center text-gray-800 mb-2 inline-block">
                     Pretty Tickets
                 </h1>
                 <img className="inline-block ml-1 sm:ml-2 -mt-10 animate-in spin-in-90 ease-in-out duration-1000" width={75} src="/logo128.png" alt="ticket" />
-            </div>
-            <h2 className="text-[20px] text-gray-800 font-bold">Elevate The Ticket-Sharing Experience<span className="text-sm absolute">™</span></h2>
+            </>
+            <h2 className="text-lg text-gray-800 font-bold">Elevate The Ticket-Sharing Experience<span className="text-sm absolute">™</span></h2>
             <p className="mt-4 mb-6 sm:my-6">Transform that ordinary event booking into an elegant, tailor-made online ticket that's perfect for gifting and sharing.</p>
             <div className="flex flex-col md:flex-row gap-4 items-center justify-center">
                 <div className="bg-white p-4 rounded-lg shadow-md text-center sm:flex-grow md:w-[200px] md:h-[110px] w-full">
