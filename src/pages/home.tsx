06b3d53e255f4bedc80340c7ed0d5445bfaecddd
Benjamin Hurley <benfromtech@gmail.com>
2023-09-15 14:20:26 -0400

    break ticket input into 3 sections

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -165,16 +165,16 @@ export const Home = () => {
         queryParams.set('mode', 'creatorMode'); // default to creator mode on submit
         queryParams.set('showConfetti', showConfetti.toString());
 
-        queryParams.set('eventName', eventName.slice(0,50) || emptyValue);
-        queryParams.set('eventSubtitle', eventSubtitle.slice(0,50) || emptyValue);
-        queryParams.set('eventDescription', eventDescription.slice(0,200) || emptyValue);
+        queryParams.set('eventName', eventName.slice(0, 50) || emptyValue);
+        queryParams.set('eventSubtitle', eventSubtitle.slice(0, 50) || emptyValue);
+        queryParams.set('eventDescription', eventDescription.slice(0, 200) || emptyValue);
         queryParams.set('eventDate', eventDate || emptyValue);
         queryParams.set('eventStartTime', eventStartTime || emptyValue);
         queryParams.set('eventEndTime', eventEndTime || emptyValue);
-        queryParams.set('eventVenue', eventVenue.slice(0,50) || emptyValue);
-        queryParams.set('eventSection', eventSection.slice(0,10) || emptyValue);
-        queryParams.set('eventRow', eventRow.slice(0,10) || emptyValue);
-        queryParams.set('eventSeat', eventSeat.slice(0,10) || emptyValue);
+        queryParams.set('eventVenue', eventVenue.slice(0, 50) || emptyValue);
+        queryParams.set('eventSection', eventSection.slice(0, 10) || emptyValue);
+        queryParams.set('eventRow', eventRow.slice(0, 10) || emptyValue);
+        queryParams.set('eventSeat', eventSeat.slice(0, 10) || emptyValue);
 
         queryParams.set('imgUrl', imgUrl || emptyValue);
         queryParams.set('ticketColor', ticketColor || emptyValue);
@@ -182,7 +182,7 @@ export const Home = () => {
         queryParams.set('textColor', textColor || emptyValue);
         queryParams.set('font', font || emptyValue);
         queryParams.set('giftMessage', giftMessage || emptyValue);
-        queryParams.set('gifterName', gifterName.slice(0,50) || 'Someone');
+        queryParams.set('gifterName', gifterName.slice(0, 50) || 'Someone');
 
         const queryString = queryParams.toString();
         const longURL = `/results?${queryString}`;
@@ -232,9 +232,8 @@ export const Home = () => {
                         <h3 className="text-lg font-semibold mb-1 text-left">Add Details</h3>
                         <p className="text-sm text-gray-600 text-left">Auto-fill event details using Generative AI.</p>
                     </div>
-                    <div className="rounded-xl shadow-xl border border-white flex flex-col justify-between max-h-[420px] md:max-h-none overflow-y-scroll pb-4 bg-white bg-opacity-30 px-4 pt-3 mx-2 sm:px-6 sm:pt-4 min-w-[250px] md:max-w-[500px]">
-                        <div className="flex-grow overflow-y-scroll overflow-x-hidden">
-                            <div className="mt-2">
+                    <div className="rounded-xl shadow-xl border border-white flex flex-col justify-between max-h-[420px] md:max-h-none overflow-y-scroll bg-white bg-opacity-30 mx-2 min-w-[250px] md:max-w-[500px]">
+                        <div className="flex-grow overflow-y-scroll overflow-x-hidden px-4 py-6">
                                 <div className="flex justify-between align-center mb-3">
                                     <h3 className="text-mg lg:text-xl font-extrabold leading-tight text-left text-gray-800 ml-2">Event Details</h3>
                                     <div className="flex gap-2 mr-2">
@@ -242,7 +241,7 @@ export const Home = () => {
                                         <Button className="bg-orange-100 text-xs border rounded-full px-4 py-1 font-semibold" onClick={handleClearEventFields}>Clear</Button>
                                     </div>
                                 </div>
-                                <p className="mx-2 my-4"><span className="font-semibold italic">Recommended:</span> Paste your email confirmation or any raw purchase details below and let Generative AI fill out the fields automatically.</p>
+                                <p className="text-sm mx-2 my-4"><span className="text-sm font-semibold italic">Recommended:</span> Paste your email confirmation or any raw purchase details below and let Generative AI fill out the fields automatically.</p>
                                 <InputTextAreaFieldWithAI
                                     label="Raw Ticket Data"
                                     value={purchaseData}
@@ -308,7 +307,9 @@ export const Home = () => {
                                     callbackFn={setEventSeat}
                                 />
                             </div>
-                            <div className="my-12">
+                    </div>
+                    <div className="rounded-xl shadow-xl border border-white flex flex-col justify-between max-h-[420px] md:max-h-none overflow-y-scroll bg-white bg-opacity-30 mt-8 mx-2 min-w-[250px] md:max-w-[500px]">
+                        <div className="flex-grow overflow-y-scroll overflow-x-hidden px-4 py-6">
                                 <div className="flex justify-between mb-2 mr-2">
                                     <h3 className="text-mg lg:text-xl font-extrabold leading-tight text-left text-gray-800 ml-2">Ticket Design</h3>
                                     <GenerateWithAIButton
@@ -347,8 +348,9 @@ export const Home = () => {
                                     callbackFn={setImgUrl}
                                 />
                             </div>
-
-                            <div className="mb-2">
+                    </div>
+                    <div className="rounded-xl shadow-xl border border-white flex flex-col justify-between max-h-[420px] md:max-h-none overflow-y-scroll bg-white bg-opacity-30 mt-8 mx-2 min-w-[250px] md:max-w-[500px]">
+                        <div className="flex-grow overflow-y-scroll overflow-x-hidden px-4 py-6">
                                 <div className="flex justify-between align-center mb-2">
                                     <h3 className="text-mg lg:text-xl font-extrabold leading-tight text-left text-gray-800 ml-2">Gift Message</h3>
                                     <div className="flex gap-2">
@@ -376,13 +378,12 @@ export const Home = () => {
                                 />
                             </div>
                         </div>
-                    </div>
                 </div>
                 <div className="flex-1 md:w-1/2 px-4 mt-4 sm:mt-0 mb-auto sticky top-16">
                     <div className="mt-4 mx-2 sm:mt-6 mb-10">
                         <div className="bg-green-100 p-4 rounded-lg shadow-md text-center sm:flex-grow max-w-[350px] w-full mx-auto mb-8">
                             <h3 className="text-lg font-semibold mb-1 text-left">Design</h3>
-                            <p className="text-sm text-gray-600 text-left">Preview your commemorative ticket below:</p>
+                            <p className="text-sm text-gray-600 text-left">Preview your commemorative ticket below</p>
                         </div>
                         <Ticket
                             eventName={eventName}
