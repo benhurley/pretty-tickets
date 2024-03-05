2bfbed0019fd2041f6680f3234e2665eb6e3358f
Ben Hurley <benfromtech@gmail.com>
2024-03-04 23:37:31 -0500

    add new justbenfyi attribution

--- src/components/organisms/footer.tsx
+++ src/components/organisms/footer.tsx
@@ -1,11 +1,40 @@
 /* eslint-disable react/jsx-no-target-blank */
+
+import Me from '../atoms/me-reversed.webp';
+
 export const Footer = () => {
     return (
         <div className=" w-full fixed bottom-0 text-sm bg-white bg-opacity-90 z-20 px-4">
-            <div className="flex items-center justify-between align-center max-w-[450px] mx-auto text-gray-800 ">
-                <a className="hover:underline" href="https://github.com/benhurley/fancy-tickets" target="_blank" rel="noreferrer">Contribute</a>
-                <p className="text-center p-2">Created with ♥ by <a className="hover:underline" target="_blank" rel="no-repeat" href="https://justben.fyi">Ben</a>, 2023.</p>
-                <a className="hover:underline" href="https://github.com/sponsors/benhurley" target="_blank" rel="noreferrer">Sponsor</a>
+            <div className="flex justify-center items-center align-center mx-auto text-gray-800 ">
+                <div
+                    className="flex place-items-center gap-1 py-2"
+                >
+                    By
+                    <a
+                        className="hover:underline mr-1"
+                        href='https://justben.fyi'
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        justben.fyi
+                    </a>
+                    <img
+                        src={Me}
+                        alt="justben.fyi logo"
+                        width={75}
+                        height={24}
+                    />
+                    <div>
+                    </div>
+                    <a
+                        className="underline"
+                        href='https://pay.justben.fyi'
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Donate
+                    </a>
+                </div>
             </div>
         </div>
     )
