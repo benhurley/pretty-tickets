1ae24b935cd429ee56d7d760d18f3037490e283b
Ben Hurley <benjaminhurley@pm.me>
2025-02-19 22:23:08 -0500

    fix: no opacity on header or footer bg

--- src/components/organisms/footer.tsx
+++ src/components/organisms/footer.tsx
@@ -4,7 +4,7 @@ import Me from '../atoms/me.webp';
 
 export const Footer = () => {
     return (
-        <div className=" w-full fixed bottom-0 text-sm bg-white bg-opacity-90 z-20 px-4">
+        <div className=" w-full fixed bottom-0 text-sm bg-white bg-opacity-100 z-20 px-4">
             <div className="flex justify-center items-center align-center mx-auto text-gray-800 ">
                 <div
                     className="flex place-items-center gap-1 py-2 pr-10"
--- src/components/organisms/header.tsx
+++ src/components/organisms/header.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 
 export const Header = () => {
     return (
-        <div className="w-full fixed top-0 z-20 text-xs bg-white bg-opacity-90">
+        <div className="w-full fixed top-0 z-20 text-xs bg-white bg-opacity-100">
             <p className="text-lg font-bold text-gray-800 text-center p-2 transform hover:scale-105 transition-transform duration-300">
                 <Link to="/">Pretty Tickets<span className="text-[10px] absolute top-0">™</span><img className="inline-block ml-4 -mt-1" width={30} src="/logo128.webp" alt="ticket" /></Link>
             </p>
